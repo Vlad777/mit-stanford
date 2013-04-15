0e4e2b713279d9c5e0e356825d023dfaefff70b1
charles <chars.long@gmail.com>
2013-04-15 00:32:42 -0700

    Few edits to MITpages.php
    Took out the need to initialize array the wrong way.
    Used the correct function to check if a value existed in the array.
    Now code iterates through all urls found in the first page.
    
    Currently MITpages.php does:
    
    course name
    category
    class link

--- MITpages.php
+++ MITpages.php
@@ -3,15 +3,15 @@ include ('simple_html_dom.php');
 
 $html = file_get_html('http://ocw.mit.edu/courses/audio-video-courses/');
 
-//too lazy too figure out how to initalize array so array[0] will currently be "nothing"
-$array[] = nothing;
-
-
 //save every URL to every course offered into array
 foreach($html->find('a[rel="coursePreview"]') as $e){
-	if(!array_search('http://ocw.mit.edu' . $e->href,$array))
+	if(empty($array)){
+		$array[] = 'http://ocw.mit.edu' . $e->href;
+		}
+	elseif(!(in_array('http://ocw.mit.edu'.$e->href,$array))){
 		$array[] = 'http://ocw.mit.edu' . $e->href;
 	}
+}
 
 
 //seeing how printing values worked for php
@@ -21,7 +21,8 @@ foreach($array as $key => $value){
 
 
 //this would be a foreach loop but for now just working on getting the first webpage
-$html = file_get_html($array[1]); 
+foreach($array as $url){
+$html = file_get_html($url);
 $e = $html->find('title',0);
 
 
@@ -32,6 +33,8 @@ $subject = $titleElement[1];
 
 echo '<br>'. "courseName: " . $courseName;
 echo '<br>'. "subject: " . $subject;
+echo '<br>'. "classURL: " . $url;
+}
 
 
 
