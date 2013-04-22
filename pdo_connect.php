754da47a41f772ce88fe1819be5e16d8a56135b0
Vlad777 <vlad777@gmail.com>
2013-04-21 22:28:44 -0700

    Create pdo_connect.php
    It is not recommended to use the old mysql extension for new development, as it has been deprecated as of PHP 5.5.0 and will be removed in the future.
    
    PDO seems like a reasonable substitute and I confirmed it works on the http://cs160.sjsu-cs.org site.

---
+++ pdo_connect.php
@@ -0,0 +1,43 @@
+<?php
+
+function dbConnect() {
+  global $dbh;
+
+  $dbInfo['database_target'] = "localhost";
+  $dbInfo['database_name'] = "db";
+  $dbInfo['username'] = "root";
+  $dbInfo['password'] = "";
+
+
+  $dbConnString = "mysql:host=" . $dbInfo['database_target'] . "; dbname=" . $dbInfo['database_name'];
+  $dbh = new PDO($dbConnString, $dbInfo['username'], $dbInfo['password']);
+  $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $error = $dbh->errorInfo();
+  if($error[0] != "") {
+      print "<p>DATABASE CONNECTION ERROR:</p>";
+      print_r($error);
+  }
+}
+
+function fetchAll($queryString) {
+  global $dbh;
+  return $dbh->query($queryString)->fetchAll();
+}
+
+dbConnect(); // Connect to Database
+
+//Sample usage:
+/*
+<?php
+require_once('pdo_connect.php');
+$query = fetchAll("SELECT * FROM course_data");
+echo "<pre>"; //for nice indented formatting of print_r
+foreach ($query as $row) { //print all rows
+  print_r($row); // prints all columns
+}
+echo "</pre>";
+?>
+
+*/
+?>
+
