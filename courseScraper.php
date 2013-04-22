bb7c961489f92d4b2e4450397107ee6e7618323f
Vlad777 <vlad777@gmail.com>
2013-04-22 02:02:34 -0700

    Create courseScraper.php

---
+++ courseScraper.php
@@ -0,0 +1,11 @@
+<?php
+
+requires_once("pdo_connect.php");
+
+execQuery("TRUNCATE TABLE course_data");
+execQuery("TRUNCATE TABLE coursedetails");
+
+include("courseScraperMIT.php");
+include("courseScraperSEE.php");
+
+?>
