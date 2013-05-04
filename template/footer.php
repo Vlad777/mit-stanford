8dd89ea36cd4ef6f050dbf122a962bef1ae67c7c
Diem Nguyen <diiemnguyen@yahoo.com>
2013-05-04 02:18:16 -0700

    Update footer.php

--- template/footer.php
+++ template/footer.php
@@ -1,5 +1,11 @@
-<div id="footer">
-
-	Copyright stuff.
-
+<div id="footer" style="margin:0 auto;width:976px;text-align:center;">
+    <div style = "padding-left:175px;">
+    <br>
+		<p>
+            <a href="index.php">Home</a>&nbsp;//
+            <a href="">Contact Us</a>&nbsp;//
+            <a href="">Privacy Policy</a>&nbsp;//
+            <a href="">Site Map</a>
+        </p>    
+  	</div>
 </div>
