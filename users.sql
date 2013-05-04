701f8f9c65369af561200b6151bb6b1fff4c5ba9
Vlad777 <vlad777@gmail.com>
2013-05-04 15:18:26 -0700

    Update users.sql
    Changing to use utf8.

--- users.sql
+++ users.sql
@@ -37,7 +37,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   PRIMARY KEY (`userid`),
   UNIQUE KEY `username` (`username`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
 
 --
 -- Dumping data for table `users`
