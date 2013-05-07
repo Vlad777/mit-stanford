0454c707da71bbb30f7915be1e02c450f5b69dbe
Diem Nguyen <diiemnguyen@yahoo.com>
2013-05-07 15:59:24 -0700

    Update footer with back to top

--- template/footer.php
+++ template/footer.php
@@ -1,11 +1,10 @@
 <div id="footer" style="margin:0 auto;width:976px;text-align:center;">
-    <center>
-    <br>
-		<p>
-            <a href="index.php">Home</a>&nbsp;//
-            <a href="">Contact Us</a>&nbsp;//
-            <a href="policy.php">Privacy Policy</a>&nbsp;//
-            <a href="">Site Map</a>
-        </p>    
-  	</center>
+    	<center>
+    	<br>
+		<a href="index.php">Home</a>&nbsp;// 
+		<a href="">Contact Us</a>&nbsp;// 
+		<a href="policy.php">Privacy Policy</a>&nbsp;// 
+		<a href="sitemap.php">Site Map</a>		
+		<a href="#top" style="margin-left:80px;">Back to Top</a>
+	</center>
 </div>
