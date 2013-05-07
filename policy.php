53fee7a3cca6d9630e5f093f9a2c7105ba1dfd3b
Diem Nguyen <diiemnguyen@yahoo.com>
2013-05-07 00:46:40 -0700

    create a new page for policy.php

---
+++ policy.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Privacy and Policy</title>
+
+<?php include("template/header.php"); ?>
+
+</head>
+
+<body>
+
+<b>Privacy</b><br />
+MIT OpenCourseWare respects your privacy. We do not collect personally identifiable information about you unless you voluntarily provide it, such as when you provide email contact information to subscribe to the OCW newsletter, send feedback to OCW, or respond to a survey. If you voluntarily provide your email address or other contact information, we might also use it to inform you of changes to OCW, to survey you about your use or opinion of OCW, or to ask for your support. At your request, we will remove your contact information from our files.<br />
+
+We do not make your contact information or any other personally identifiable information available to anyone outside OCW or its service providers (who use the information only for authorized OCW purposes) unless we are legally required to do so.<br />
+
+In addition to the above, we collect certain anonymous (non-personally identifiable) information to help us improve the OCW web site and to evaluate the access and use of OCW materials and the impact of OCW on the worldwide educational community:<br />
+<ul>
+<li>We collect information you provide about your use of and satisfaction with OCW through email you send us, through the OCW feedback form, and through OCW surveys, whether or not you voluntarily include your contact information.</li>
+<li>We may use web analysis tools that are built into the OCW web site to measure and collect anonymous session information.</li>
+<li>We also use "cookies" to improve your OCW web experience and to collect anonymous information about how you use OCW. However, cookies are not required for OCW use. If your browser is configured not to accept cookies, you will still be able to access OCW and its content.</li>
+</ul>
+<br />
+When we report information about OCW access, use, and impact, we report aggregate, non-personally identifiable data. Occasionally, we report quoted feedback from users. We do not attribute feedback to specific individuals unless we obtain permission to use that person's name along with the feedback.<br />
+
+
+<?php include("template/footer.php"); ?>
+</body>
+</html>
