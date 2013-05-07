99442850a311e2a76a96deee2a21b6a60b88583c
Vlad <vlad777@gmail.com>
2013-05-06 22:44:42 -0700

    Integrating search into main index.php

--- index.php
+++ index.php
@@ -9,21 +9,93 @@
 //require_once('connection.php');
 require_once('pdo_connect.php');
 
-$queryString = 'SELECT d.id, d.title, d.short_desc, 
+
+if(isset($_POST['Submit']))
+{
+  //This should really be implemented using Zend/Lucene indexing engine
+
+	//Using MATCH FULLTEXT indexing requires table to be MyISAM, not INNODB!!
+	//this matches only the words searched, not partial strings
+	//$qrm = $dbh->prepare("SELECT * FROM course_data WHERE MATCH(title,long_desc,category) AGAINST (?)");
+	//$qrm->execute(array($_POST["q"]));
+
+	//this allows partial word matches by using BOOLEAN MODE... if we want this? actually slower than using LIKE '%keyword%'
+    //$qrm = $dbh->prepare("SELECT * FROM course_data WHERE MATCH(title,long_desc,category) AGAINST (? in BOOLEAN MODE)");
+	//$qrm->execute(array('*' . $_POST["q"] . '*'));
+
+	//Using LIKE with % at the beginning and with ORs is NOT SCALABLE
+	//The problem here is that this would return more results than the autocomplete label shows, since autocomplete is based on words, not substrings of words
+	//At least this should work with INNODB as well as MyISAM for now...
+	$qrm = $dbh->prepare("SELECT * FROM course_data WHERE title LIKE ? OR long_desc LIKE ? OR category LIKE ?");
+	$qrm->execute(array('%' . $_POST["q"] . '%', '%' . $_POST["q"] . '%', '%' . $_POST["q"] . '%'));
+
+	$results = $qrm->fetchAll();
+	// echo "<pre>"; //for nice indented formatting of print_r
+	// print_r($courses);
+	// echo "</pre>";
+}
+else
+{
+	$queryString = 'SELECT d.id, d.title, d.short_desc, 
 				d.course_link, d.video_link, d.course_length, d.course_image, 
 				d.category, d.start_date, d.site FROM course_data d';
-
-$results = fetchAll($queryString);
+	$results = fetchAll($queryString);
+}
 ?>
 <!DOCTYPE html>   
 <html>
 <head>
+
+
 <title>MOOCS mashup | MIT + SEE | CS 160 Team 3</title>
+<meta charset="utf-8" />
 <script  type="text/javascript" src="includes/js/sorttable.js"></script>
- <script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
+
+<link rel="stylesheet" href="http://code.jquery.com/ui/1.10.3/themes/smoothness/jquery-ui.css" />
+
+<script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
+
+<!-- <script src="http://code.jquery.com/jquery-1.9.1.js"></script> 
+ -->
+
+<!-- Has to be after jquery.min.js? hmm -->
+<script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
+
 <link rel="stylesheet" href="template/style.css" />	
+
+
+<script>
+$(function() {
+var availableTags = [
+
+
+<?php
+function sub($arr)
+{
+  return $arr[0];
+}
+require_once('pdo_connect.php');
+$array = fetchAll("SELECT concat(concat(concat(word, ' ('), course_count), ')') as word FROM autocomplete ORDER BY course_count DESC");
+//print_r(implode(array_map("sub", $array)));
+echo '"' . implode('","', array_map("sub", $array)) . '"';
+?>
+
+
+];
+$( "#tags" ).autocomplete({
+source: availableTags,
+select: function(event, ui) { ui.item.value = ui.item.value.split(" (")[0]; }
+});
+});
+</script>
+
+
+
 </head>
 <body>
+
+
+
 <script type="text/javascript">
 function open_video(url)
 {
--- template/header.php
+++ template/header.php
@@ -5,6 +5,20 @@
 		<span class="gold">MIT + Stanford </span><span class="blue"><b>Mashup</b></span>
    </div>     
  
+<div class="ui-widget">
+<form id="search" action="index.php" method="post">
+<label for="tags"></label>
+<input name="q" id="tags" autofocus />
+<script>
+if (!("autofocus" in document.createElement("input"))) {
+ document.getElementById("tags").focus();
+}
+</script>
+<input type="submit" name="Submit" value="Search" />
+</form>
+</div>
+
+
     <?php		
 	include("includes/user_test.php");	?>
 </div>
