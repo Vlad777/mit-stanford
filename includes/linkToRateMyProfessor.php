2b1f214193b9e6e9f40ee8e07bbf911614da2c53
Alicedelic <alice@cotti.biz>
2013-05-07 18:35:16 -0700

    Made Header smaller
    File changed:
    
    includes/user_test.php
    template/style.css
    template/header.php
    images/popup-close.png
    includes/linkToRateMyProfessor.php  (hid debug text)

---
+++ includes/linkToRateMyProfessor.php
@@ -0,0 +1,81 @@
+<?
+/*********************************************
+* includes/linkToRateMyProfessor.php
+* @Author Alice Cotti
+* @date 05/03/2013
+* @ CS 160 SJSU Spring 2013
+*
+**********************************************/
+
+function linkToRateMyProfessor($profName, $siteName)
+{
+	$prof = $profName;
+	//$replace = array('Dr.' => '', 'Prof.'=>'','Biological Engineering Faculty'=>'','\(*\)'=>'');
+  	$prof = str_replace('Dr. ','',$prof);
+  	$prof = str_replace('Prof. ','',$prof);
+  	$prof = str_replace('Biological Engineering Faculty','',$prof);
+  	$prof = preg_replace('/\(*\)/','',$prof);
+	
+	// example URL to fetch JSON data
+	// http://www.ratemyprofessors.com/solr/interim.jsp?select?facet=true&q=Mehran%20Sahami&wt=json
+	$reqResults =  file_get_contents('http://www.ratemyprofessors.com/solr/interim.jsp?select?facet=true&q='. urlencode($prof) . '&wt=json');
+	$data = json_decode($reqResults, FALSE);
+	
+	$foundName = 0;
+	$foundSchool = 0;	
+  
+	//this could be a while loop to try more combinatiion of partial name?
+	if ($data->response->numFound == 0)
+	{
+		//if profname can be split in 3 parts, try to get the second name initial out of the search
+		$nameArray = explode(' ',$prof);
+		//print_r($nameArray);
+		if (count($nameArray) > 2)
+		{
+			$reqResults =  file_get_contents('http://www.ratemyprofessors.com/solr/interim.jsp?select?facet=true&q='. 
+											 urlencode($nameArray[0] .' '.$nameArray[2]) . '&wt=json');
+			$data = json_decode($reqResults, FALSE);
+		}		
+	}
+	
+	if ($data->response->numFound == 1)
+	{
+		$foundName = 1;
+		$aLink = 'http://www.ratemyprofessors.com/ShowRatings.jsp?tid='. $data->response->docs[0]->pk_id;
+		echo '<a onclick="open_video(\''.$aLink.'\');return false;" href="'.$aLink.'" target="_blank">Link to Rate My Professor</a>';
+		$foundSchool = 1;
+	}
+	else if ($data->response->numFound > 1) //if more than one match
+	{
+		$foundName = 1;
+		$docs = $data->response->docs;	
+		//foreach school, matching profname
+		foreach ($docs as $school)
+		{
+			$schoolname = $school->schoolname_s;			
+			if ($siteName == 'SEE')
+					$pattern = '/Stanford/';
+			else if ($siteName == 'MIT')
+					$pattern = '/Massachusetts Institute of Technology/';
+			else {
+				echo ' missing school name for this site name <br />';	
+			}			
+			preg_match($pattern, $schoolname, $matches);		
+			if ( count($matches) >= 1)
+			{
+				$aLink = 'http://www.ratemyprofessors.com/ShowRatings.jsp?tid='. $school->pk_id;
+				echo '<a onclick="open_video(\''.$aLink.'\');return false;" href="'.$aLink.'" target="_blank">Link to Rate My Professor</a>';
+				$foundSchool = 1;
+			}		
+		} //foreach
+	} 	
+	
+	 // mostly for debug purposes
+	 /*
+		if (!$foundName)
+			echo 'Could not find matching instructor name<br />';	
+		else if (!$foundSchool)
+			echo 'Could not find matching school name<br />';		
+ 	*/
+}
+?>
--- template/header.php
+++ template/header.php
@@ -2,24 +2,31 @@
 
 	<div id="logo">
     	<h1><span class="blue">Moocs</span><span class="gold">4u</span></h1>
-		<span class="gold">MIT + Stanford </span><span class="blue"><b>Mashup</b></span>
-   </div>     
- 
-<div class="ui-widget">
-<form id="search" action="index.php" method="post">
-<label for="tags"></label>
-<input name="q" id="tags" autofocus />
-<script>
-if (!("autofocus" in document.createElement("input"))) {
- document.getElementById("tags").focus();
-}
-</script>
-<input type="submit" name="Submit" value="Search" />
-<input type="hidden" name="do" value="search" />
-</form>
-</div>
+		<!-- <span class="gold">MIT + Stanford </span><span class="blue"><b>Mashup</b></span> -->
+   </div>  
+    <?php  include("includes/user_test.php");	?>
+    
+    
+    <div class="ui-widget" id="searchBox">
+        <form id="search" action="index.php" method="post">
+        <label for="tags"></label>
+        <input name="q" id="tags" autofocus />
+        <script>
+        if (!("autofocus" in document.createElement("input"))) {
+         document.getElementById("tags").focus();
+        }
+        </script>
+        <input type="submit" name="Submit" value="Search" />
+        </form>
+    </div>
+    
+    <div id="login-box" class="popup" style="display:none;">
+     <a class="closebutton" onClick="$('#login-box').fadeOut();" alt="close" title="close"></a>
+   	 <?  include("includes/login.php"); ?>        
+  </div>
+  <div id="register-box" class="popup" style="display:none;">
+     <a class="closebutton" onClick="$('#register-box').fadeOut();" alt="close" title="close"></a>
+  	 <?  include("includes/register.php"); ?>   
+  </div> 
 
-
-    <?php		
-	include("includes/user_test.php");	?>
 </div>
