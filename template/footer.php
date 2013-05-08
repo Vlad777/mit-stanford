8e4214adf2aeae071330251f962217f4a7ae1c00
Diem Nguyen <diiemnguyen@yahoo.com>
2013-05-07 18:45:49 -0700

    Done footer.php - will post contactus.php and sitemap.php when they are done

--- template/footer.php
+++ template/footer.php
@@ -1,10 +1,10 @@
 <div id="footer" style="margin:0 auto;width:976px;text-align:center;">
-    	<center>
-    	<br>
+    <center>
+    <br>
 		<a href="index.php">Home</a>&nbsp;// 
-		<a href="">Contact Us</a>&nbsp;// 
+		<a href="contactus.php">Contact Us</a>&nbsp;// 
 		<a href="policy.php">Privacy Policy</a>&nbsp;// 
 		<a href="sitemap.php">Site Map</a>		
 		<a href="#top" style="margin-left:80px;">Back to Top</a>
-	</center>
+  	</center>
 </div>
