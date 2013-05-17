4d49b55a89a2ffe728aa008064b55ea3d3ea77fd
Diem Nguyen <diiemnguyen@yahoo.com>
2013-05-17 14:10:19 -0600

    Update contactus.php

--- contactus.php
+++ contactus.php
@@ -11,9 +11,22 @@
 
 <div style="border: 2px solid green ;">
 <div style="margin-left:60px;margin-right:60px;"><div style="font-size:18px;"><b>Contact Us</b></div><br />
-  <div style="text-indent:20px;"></div>
+  <div>
   
-	
+Have questions for us? Need more information? We're here to help. Easily find the right contact below for all your inquiries.<br /><br />
+
+If you're visiting our website for the first time, let us know if we can help you in any way. Our experienced team of professionals will find the answers to help you achieve your successes.<br /><br />
+
+<b>Contact Information:</b><br />
+<div style="text-indent:20px;">Mooc4u</div>
+<div style="text-indent:20px;">Group: Three Tree</div>
+<div style="text-indent:20px;">San Jose Sate University</div>
+<div style="text-indent:20px;">1 Washington Sq</div>
+<div style="text-indent:20px;">San Jose, CA 95192</div>
+
+
+  
+  </div>
 </div>
 </div>
 
