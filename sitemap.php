223c08f56405d94cac298526fa3149d03acb9ee8
Diem Nguyen <diiemnguyen@yahoo.com>
2013-05-17 13:55:59 -0600

    Create sitemap.php

---
+++ sitemap.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Privacy and Policy</title>
+
+</head>
+
+<body>
+
+<?php include("template/header.php"); ?>
+
+<div style="border: 2px solid green ;">
+<div style="margin-left:60px;margin-right:60px;"><div style="font-size:18px;"><b>Sitemap</b></div><br />
+  <div style="text-indent:20px;"></div>
+  
+	
+</div>
+</div>
+
+<?php include("template/footer.php"); ?>
+</body>
+</html>
