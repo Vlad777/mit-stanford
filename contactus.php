5473da15ba8e6b461973499d239ef7c90acf95a0
Diem Nguyen <diiemnguyen@yahoo.com>
2013-05-17 23:19:25 -0600

    Done contactus.php

--- contactus.php
+++ contactus.php
@@ -10,19 +10,23 @@
 <?php include("template/header.php"); ?>
 
 <div style="border: 2px solid green ;">
-<div style="margin-left:60px;margin-right:60px;"><div style="font-size:18px;"><b>Contact Us</b></div><br />
-    <div>
+<div style="margin-left:60px;margin-right:60px;"><div style="font-size:20px;"><b>Contact Us</b></div><br />
+  	<div>
 		Have questions for us? Need more information? We're here to help. Easily find the right contact below for all your inquiries.<br /><br />
 		
-		If you're visiting our website for the first time, let us know if we can help you in any way. Our experienced team of professionals will find the answers to help you achieve your successes.<br /><br />
+		If you're visiting our website for the first time, let us know if we can <a href="help.php">help</a> you in any way. Our experienced team of professionals will find the answers to help you achieve your successes.<br /><br />
+		
+		©2013 SJSU<br />
+		Report a problem with this website<br /><br />
 		
 		<b>Contact Information:</b><br />
 		<div style="text-indent:20px;">Mooc4u</div>
 		<div style="text-indent:20px;">Group: Three Tree</div>
 		<div style="text-indent:20px;">San Jose Sate University</div>
-		<div style="text-indent:20px;">1 Washington Sq</div>
+		<div style="text-indent:20px;">One Washington Square</div>
 		<div style="text-indent:20px;">San Jose, CA 95192</div>
-		
+		<div style="text-indent:20px;">408-924-1000</div>
+			
 		<br />
   	</div>
 </div>
