5fd832c7d8648eae4e07a56e7ce078146500601e
Diem Nguyen <diiemnguyen@yahoo.com>
2013-05-17 23:23:46 -0600

    Create help.php

---
+++ help.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Privacy and Policy</title>
+
+</head>
+
+<body>
+
+<?php include("template/header.php"); ?>
+
+<div style="border: 2px solid green ;">
+<div style="margin-left:60px;margin-right:60px;"><div style="font-size:20px;"><b>Help to Start</b></div><br />
+    <div>
+		
+			
+		<br />
+  	</div>
+</div>
+</div>
+
+<?php include("template/footer.php"); ?>
+</body>
+</html>
