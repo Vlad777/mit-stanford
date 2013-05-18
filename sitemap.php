d3eb71753c640af725c6b555f72f1e946fa4238f
Diem Nguyen <diiemnguyen@yahoo.com>
2013-05-17 23:12:09 -0600

    Update sitemap.php

--- sitemap.php
+++ sitemap.php
@@ -10,10 +10,38 @@
 <?php include("template/header.php"); ?>
 
 <div style="border: 2px solid green ;">
-<div style="margin-left:60px;margin-right:60px;"><div style="font-size:18px;"><b>Sitemap</b></div><br />
-  <div style="text-indent:20px;"></div>
-  
-	
+<div style="margin-left:60px;margin-right:60px;"><div style="font-size:20px;"><b>Sitemap</b></div><br />
+	<div style="text-indent:20px;">
+		<img src="images/ctdot.gif" width="22" height="22" align="absmiddle" hspace="5" border="0">
+			<span style="vertical-align:bottom;"> <a href="index.php">Home</a></span>
+		</img>
+	</div>
+
+	<div style="text-indent:20px;">
+		<img src="images/ctdot.gif" width="22" height="22" align="absmiddle" hspace="5" border="0">
+			<span style="vertical-align:bottom;"> <a href="aboutus.php">About Mooc4u</a></span>
+		</img>
+	</div>
+
+	<div style="text-indent:20px;">
+		<img src="images/ctdot.gif" width="22" height="22" align="absmiddle" hspace="5" border="0">
+			<span style="vertical-align:bottom;"> <a href="help.php">Help to start</a></span>
+		</img>
+	</div>
+
+	<div style="text-indent:20px;">
+		<img src="images/ctdot.gif" width="22" height="22" align="absmiddle" hspace="5" border="0">
+			<span style="vertical-align:bottom;"> <a href="contactus.php">Contact Us</a></span>
+		</img>
+	</div>
+
+	<div style="text-indent:20px;">
+		<img src="images/ctdot.gif" width="22" height="22" align="absmiddle" hspace="5" border="0">
+			<span style="vertical-align:bottom;"> <a href="policy.php">Privacy Policy</a></span>
+		</img>
+	</div>
+
+	<br />
 </div>
 </div>
 
