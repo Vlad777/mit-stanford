57a06255eadca4b78621480b0b21d61ed2e9c011
Meena Kaur <meenakaur86@gmail.com>
2013-05-19 20:45:17 -0700

    edited footer

--- template/footer.php
+++ template/footer.php
@@ -2,9 +2,10 @@
     <center>
     <br>
 		<a href="index.php">Home</a>&nbsp;|&nbsp;
-		<a href="contactus.php">Contact Us</a>&nbsp;|&nbsp;
+              <a href="aboutus.php">About Us</a>&nbsp;|&nbsp;
 		<a href="policy.php">Privacy Policy</a>&nbsp;|&nbsp;
-		<a href="sitemap.php">Site Map</a>		
+              <a href="contactus.php">Contact Us</a>&nbsp;|&nbsp;
+		<a href="sitemap.php">Site Map</a>	         
 		<a href="#top" style="margin-left:80px;">Back to Top</a>
   	</center>
 </div>
