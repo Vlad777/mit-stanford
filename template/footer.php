1fc7622796f882f7697f682783119f3af1248056
Alicedelic <alice@cotti.biz>
2013-05-20 16:54:18 -0700

    Changed footer

--- template/footer.php
+++ template/footer.php
@@ -6,9 +6,7 @@
 		<a href="policy.php">Privacy Policy</a>&nbsp;|&nbsp;
               <a href="contactus.php">Contact Us</a>&nbsp;|&nbsp;
 		<a href="sitemap.php">Site Map</a>	         
-		<a href="#top" style="margin-left:80px;">Back to Top</a>
+		<a href="#top" style="margin-left:80px;">Back to Top</a> &nbsp;&nbsp; &copy; 2013 SJSU
   	</center>
-     <center>    <br>
-     &copy; 2013 SJSU
-     </center>
+
 </div>
