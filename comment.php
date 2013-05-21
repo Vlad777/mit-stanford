a43383f463ae33c5249320cb2c5e8411b78081fe
Alicedelic <alice@cotti.biz>
2013-05-20 23:39:02 -0700

    Styled Comment.php page

--- comment.php
+++ comment.php
@@ -2,8 +2,18 @@
 //Comments
 require_once('pdo_connect.php');
 include("includes/function_user.php");	
+?>
+<!DOCTYPE html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Comments | Moocs4U </title>
 
+<link rel="stylesheet" href="template/style.css" />	
 
+</head>
+
+<body>
+<?
 
 if(isset($_POST['Submit']))
 {
@@ -25,26 +35,47 @@ if(isset($_POST['Submit']))
 		$st->execute(array($_GET["id"]));
 		$results = $st->fetchAll();
 
+
+      
+		 $array = fetchAll('SELECT d.id, d.title, d.site FROM course_data d WHERE d.id='. $_GET['id']);
+		?>
+        <div class="comments_page">
+		<h2><? echo $array[0]['title'] ?></h2>
+        <div style="height:auto;width:100%;border:solid 1px #9CF; margin: 0 0 130px;">
+        <?
+		 
 		foreach ($results as $row) {
 			echo "<strong>".$row["username"]."</strong> - ".$row["datesubmitted"]."<br/>";
-			echo "<div style='border-bottom:1px solid #000;margin-bottom:20px'>";
+			echo "<div style='border-bottom:1px solid  #9CF;'>";
 			echo $row["message"];
 			echo "</div>";
 		}
-
+	echo "</div>";
+	
+	
 	} else {
 		echo "No course selected.";
 	}
 	
 
 }
-
-
-
 ?>
-
-<form method="post" action="comment.php?id=<?php echo $_GET["id"]; ?>">
-	<input type="hidden" name="courseid" value="<?php echo $_GET["id"]; ?>" />
-	<textarea name="message"></textarea>
-	<input type="submit" name="Submit">
-</form>
+    <div style="position:fixed;bottom:0px;background:#FFF;width:100%;padding:6px;">    
+		<? if($user["userid"] == 0)
+        {
+            echo '<h3>You Must Login to leave a comment</h3>';
+        }
+        else {
+        ?>
+        
+        <h3>Leave a comment:</h3>
+        <form method="post" action="comment.php?id=<?php echo $_GET["id"]; ?>">
+            <input type="hidden" name="courseid" value="<?php echo $_GET["id"]; ?>" />
+            <textarea name="message" style="height:50px;width:270px;"></textarea><br />
+            <input type="submit" name="Submit">
+        </form>
+        <? } ?>
+    </div>
+</div>
+</body>
+</html>
